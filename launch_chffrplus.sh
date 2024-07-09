4f58db943d847307ce922d73af0cdccbee78a193
Rick Lan <ricklan@gmail.com>
2024-07-09 17:12:15 +0800

    fix import check

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -77,13 +77,13 @@ function launch {
   if [ -f /AGNOS ]; then
     agnos_init
     # dp
-    if ! pip show "rtree" > /dev/null 2>&1; then
-      pip install "$DIR/dp_ext/selfdrive/tetoo/pkgs/Rtree-1.2.0-cp311-cp311-linux_aarch64.whl"
+    if ! (LD_LIBRARY_PATH=$DIR/third_party/libspatialindex/larch64/lib/ python -c "import rtree") &> /dev/null; then
+      pip install "$DIR/dp_ext/selfdrive/tetood/pkgs/Rtree-1.2.0-cp311-cp311-linux_aarch64.whl"
     fi
   else
     # dp
-    if ! pip show "rtree" > /dev/null 2>&1; then
-      pip install "$DIR/dp_ext/selfdrive/tetoo/pkgs/Rtree-1.2.0-cp312-cp312-linux_x86_64.whl"
+    if ! (LD_LIBRARY_PATH=$DIR/third_party/libspatialindex/x86_64/lib/ python -c "import rtree") &> /dev/null; then
+      pip install "$DIR/dp_ext/selfdrive/tetood/pkgs/Rtree-1.2.0-cp312-cp312-linux_x86_64.whl"
     fi
   fi
 
