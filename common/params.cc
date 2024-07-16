a260c9ac04a1ccdc3c95d1904d2c401992803e27
Rick Lan <ricklan@gmail.com>
2024-07-16 16:40:30 +0800

    prep for pac

--- common/params.cc
+++ common/params.cc
@@ -246,6 +246,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"dp_device_auto_shutdown", PERSISTENT},
     {"dp_device_auto_shutdown_in", PERSISTENT},
     {"dp_device_audible_alert_mode", PERSISTENT},
+    {"dp_long_pac", PERSISTENT},
+    {"dp_long_pac_vals", PERSISTENT},
 };
 
 } // namespace
