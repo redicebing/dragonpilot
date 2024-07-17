cb29e7ffc92040706270505ba7b10d37974eddad
Rick Lan <ricklan@gmail.com>
2024-07-17 19:40:43 +0800

    limited branches to those available from updated

--- selfdrive/ui/qt/offroad/software_settings.cc
+++ selfdrive/ui/qt/offroad/software_settings.cc
@@ -64,11 +64,12 @@ SoftwarePanel::SoftwarePanel(QWidget* parent) : ListWidget(parent) {
   connect(targetBranchBtn, &ButtonControl::clicked, [=]() {
     auto current = params.get("GitBranch");
     QStringList branches = QString::fromStdString(params.get("UpdaterAvailableBranches")).split(",");
-    for (QString b : {current.c_str(), "devel-staging", "devel", "nightly", "master-ci", "master"}) {
-      auto i = branches.indexOf(b);
-      if (i >= 0) {
-        branches.removeAt(i);
-        branches.insert(0, b);
+    if (!branches.isEmpty()) {
+      QString current = QString::fromStdString(params.get("CurrentBranch"));
+      int currentIndex = branches.indexOf(current);
+      if (currentIndex >= 0) {
+        branches.removeAt(currentIndex);
+        branches.insert(0, current);
       }
     }
 
